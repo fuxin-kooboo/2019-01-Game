9e43d9bd721533779cb1fd3f95a39b10a2f5fc19
fuxin-kooboo <2786823601@qq.com>
2019-02-27 10:15:47 +0800

    initial create

---
+++ my-app/src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="app-header">
+    111111
+  </header>
+</template>
+<script>
+export default {
+  name: 'app-header'
+}
+</script>
+<style lang="scss">
+.app-header {
+  font-size: 20px;
+}
+</style>
